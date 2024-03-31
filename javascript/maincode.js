4ca75a966a46feb76d5da6743ce8a65358010eaa
Micky <165527380+rmcky0@users.noreply.github.com>
2024-03-31 22:12:49 +0800

    Update maincode.js

--- javascript/maincode.js
+++ javascript/maincode.js
@@ -6,7 +6,26 @@ const body = document.querySelector("body"),
   
 toggle.addEventListener("click", () => {
   sidebar.classList.toggle("close");
-  topnav.classList.toggle("close");
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+  // Get the current page's filename
+  var url = window.location.pathname;
+  var filename = url.substring(url.lastIndexOf('/') + 1);
+
+  // Get all navigation links
+  var navLinks = document.querySelectorAll('.nav-link a');
+
+  // Loop through each navigation link
+  navLinks.forEach(function(navLink) {
+    // Get the href attribute of the navigation link
+    var href = navLink.getAttribute('href');
+
+    // If the href attribute matches the current page's filename
+    if (filename === href) {
+      // Add the active class to the parent list item
+      navLink.parentElement.classList.add('active');
+    }
+  });
+});
 
